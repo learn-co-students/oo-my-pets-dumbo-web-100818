a3e0b4e4d6bc16b8ecc784fc284ed3e600d07505
github username <github email address>
2018-10-12 16:38:05 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,7 +73,8 @@ def sell_pets
   end
 
   def list_pets
-    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+    "hello!"
+    #{}"I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 
 
