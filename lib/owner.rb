d9e819ef56f272138cb32384d1edca4ddff3eb7a
github username <github email address>
2018-10-13 22:24:35 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  attr_reader :owner
+  attr_writer :owner, :species
+  # attr_reader: :species
+  @@owners = []
+  def initialize(owner)
+    @owner = owner
+    @species
+    @@owners << self
+  end
+
+  def species
+    @species
+  end
+
+
+
 end
