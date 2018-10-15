f19f6b8bab349be5cd21de8c4cb815d2fec28436
github username <github email address>
2018-10-15 00:48:00 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,1 @@
-Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+ Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
+require 'pry'
 class Owner
   # code goes here
+
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@people = []
+  def initialize (species)
+    @species = species
+    @@people << self
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+
+  end
+
+# binding.pry
+  def self.all
+    @@people
+  end
+
+  def self.count
+    @@people.count
+  end
+
+  def self.reset_all
+    @@people = []
+  end
+
+
+  def say_species
+    "I am a #{@species}."
+    # binding.pry
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish (name)
+    pets[:fishes] << Fish.new(name)
+    binding.pry
+    0
+  end
+
+  def buy_cat (name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog (name)
+
+    pets[:dogs] << Dog.new(name)
+  end
+
+
+
+
+
 end
